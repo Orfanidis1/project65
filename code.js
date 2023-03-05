609528895e8f5dc28084909a54a80f50fb6ce141
Orfanidis1 <125228441+Orfanidis1@users.noreply.github.com>
2023-03-05 06:29:44 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,7 @@
+var images=["https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_36/3505090/210911-gorilla-zoo-mb-1452.jpg","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/14/5d/d5/f0/giant-panda-xi-lun-born.jpg?w=1200&h=1200&s=1","http://cdn2.atlantamagazine.com/wp-content/uploads/sites/12/2017/05/Sumatran-tiger-Emerson_Zoo-Atlanta.jpg","https://img2.10bestmedia.com/Images/Photos/29956/p-zoo-atlanta-atlanta-ga-usa-attractions-family-vacations-zoos-531547_55_660x440_201404181700.jpg","https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/2d/02/18.jpg","https://www.wsav.com/wp-content/uploads/sites/75/2021/10/Abu-Zoo-Atlanta.jpg","https://www.exploregeorgia.org/sites/default/files/styles/listing_slideshow/public/listing_images/profile/4076/zooatlanta2.jpg?itok=ZNigc950"
+];
+var i=0;
+function next(){
+    document.getElementById("img").src=images[i];
+    i++
+}
